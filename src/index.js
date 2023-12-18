dffdddb35ee83ae736f8f621014d9c16c517bbf3
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-18 23:08:27 +0100

    update + fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import ImageApi from './fetch-images';
 import Notiflix from 'notiflix';
@@ -82,25 +83,26 @@ function createImageCard(imageCard) {
         comments,
         downloads,
       }) => {
-        return `<div class="photo-card">
-        <a href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        </a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes: <span>${likes}</span></b>
-          </p>
-          <p class="info-item">
-            <b>Views: <span>${views}</span></b>
-          </p>
-          <p class="info-item">
-            <b>Comments: <span>${comments}</span></b>
-          </p>
-          <p class="info-item">
-            <b>Downloads: <span>${downloads}</span></b>
-          </p>
-        </div>
-      </div>`;
+        return `
+          <div class="photo-card">
+            <a href="${largeImageURL}">
+              <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes: <span>${likes}</span></b>
+              </p>
+              <p class="info-item">
+              <b>Views: <span>${views}</span></b>
+              </p>
+              <p class="info-item">
+              <b>Comments: <span>${comments}</span></b>
+              </p>
+              <p class="info-item">
+              <b>Downloads: <span>${downloads}</span></b>
+              </p>
+            </div>
+          </div>`;
       }
     )
     .join('');
