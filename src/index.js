1b459c293745811e56fbcbeadd0303442480ed25
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-19 12:53:09 +0100

    update without copy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './styles.css';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import ImageApi from './fetch-images';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
@@ -13,44 +11,35 @@ const imageApi = new ImageApi();
 
 searchForm.addEventListener('submit', onSearch);
 loadMoreButton.addEventListener('click', onLoadMore);
-
 loadMoreButton.classList.add('is-hidden');
+
+function showMessage(type, message) {
+  Notiflix.Notify[type](message);
+}
 
 async function onSearch(event) {
   event.preventDefault();
-  clearGallery();
 
   imageApi.query = event.currentTarget.elements.searchQuery.value;
   imageApi.resetPage();
   imageApi.resethitsCounter();
 
-  if (imageApi.searchQuery === '') {
-    return;
-  }
+  if (!imageApi.query) return;
 
-  const imgResponse = await imageApi.fetchImages();
   try {
-    if (imgResponse.totalHits === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      loadMoreButton.classList.add('is-hidden');
-    }
-    if (imgResponse.totalHits > 0) {
-      Notiflix.Notify.success(
-        `Hooray! We found ${imgResponse.totalHits} images!`
-      );
+    const imgResponse = await imageApi.fetchImages();
 
+    if (imgResponse.totalHits === 0) {
+      showMessage('failure', 'Sorry, there are no images matching your search query. Please try again.');
+      loadMoreButton.classList.add('is-hidden');
+    } else {
+      showMessage('success', `We found ${imgResponse.totalHits} images!`);
       loadMoreButton.classList.remove('is-hidden');
       createImageCard(imgResponse.hits);
     }
-    if (
-      (imageApi.viewedHits === imageApi.totalHits) &
-      (imgResponse.totalHits !== 0)
-    ) {
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
+
+    if (imageApi.viewedHits === imageApi.totalHits && imgResponse.totalHits !== 0) {
+      showMessage('info', "You've reached the end of search results.");
       loadMoreButton.classList.add('is-hidden');
     }
   } catch (error) {
@@ -59,66 +48,49 @@ async function onSearch(event) {
 }
 
 async function onLoadMore() {
-  const imgResponse = await imageApi.fetchImages();
-  if (imageApi.viewedHits === imageApi.totalHits) {
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
-    loadMoreButton.classList.add('is-hidden');
-  }
-  createImageCard(imgResponse.hits);
+  try {
+    const imgResponse = await imageApi.fetchImages();
 
-  autoScroll();
+    if (imageApi.viewedHits === imageApi.totalHits) {
+      showMessage('info', "You've reached the end of search results.");
+      loadMoreButton.classList.add('is-hidden');
+    }
+
+    createImageCard(imgResponse.hits);
+    autoScroll();
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 function createImageCard(imageCard) {
-  const markupList = imageCard
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-          <div class="photo-card">
-            <a href="${largeImageURL}">
-              <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </a>
-            <div class="info">
-              <p class="info-item">
-                <b>Likes: <span>${likes}</span></b>
-              </p>
-              <p class="info-item">
-              <b>Views: <span>${views}</span></b>
-              </p>
-              <p class="info-item">
-              <b>Comments: <span>${comments}</span></b>
-              </p>
-              <p class="info-item">
-              <b>Downloads: <span>${downloads}</span></b>
-              </p>
-            </div>
-          </div>`;
-      }
-    )
-    .join('');
+  const markupList = imageCard.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+    <div class="photo-card">
+      <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: <span>${likes}</span></b>
+        </p>
+        <p class="info-item">
+          <b>Views: <span>${views}</span></b>
+        </p>
+        <p class="info-item">
+          <b>Comments: <span>${comments}</span></b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: <span>${downloads}</span></b>
+        </p>
+      </div>
+    </div>`).join('');
 
   gallery.insertAdjacentHTML('beforeend', markupList);
-  const lightbox = new SimpleLightbox('.gallery a');
-}
-
-function clearGallery() {
-  gallery.innerHTML = '';
+  new SimpleLightbox('.gallery a');
 }
 
 function autoScroll() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
+  const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
 
   window.scrollBy({
     top: cardHeight * 2,
